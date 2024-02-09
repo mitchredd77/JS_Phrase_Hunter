0475a09597b6479dcd8a3d919c52be99e2ba03fa
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2024-02-09 07:46:16 -0700

    Fixed Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+      this.phrase = phrase.toLowerCase();
+      this.letters = this.phrase.split('');
+      this.correctLetters = [];
+    }
+    addPhraseToDisplay() {
+      const ul = document.querySelector('ul');
+      // Split the phrase and create list items
+      this.letters.forEach(letter => {
+        if (letter === ' ') {
+            ul.insertAdjacentHTML('beforeend', `<li class="space">${letter}</li>`);
+        } else {
+           ul.insertAdjacentHTML('beforeend', `<li class="hide letter ${letter}">${letter}</li>`);
+        }
+        
+      });
+    }
+    checkLetter(value) {
+        this.letters.forEach(letter => {
+            if (letter === value) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+    showMatchedLetter() {
+      const allLetters = document.querySelectorAll('.letter')
+      allLetters.forEach(letter => {
+          console.log(letter.textContent);
+          if (this.checkLetter(letter.textContent)) {
+            letter.classList.remove('hide')
+            letter.classList.add('show');
+            
+            }
+    
+      });
+    }
+    }
+
+const test = new Phrase("How are you");
+test.addPhraseToDisplay();
+
+  
+
